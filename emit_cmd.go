072ace1d9f05fa6b2b8fe260bb8db5d0009579ba
David Nix <hello@davidnix.io>
2023-03-26 12:03:36 -0600

    Start to piece together the emit command

--- emit_cmd.go
+++ emit_cmd.go
@@ -1,14 +1,45 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"time"
+
+	"github.com/DavidNix/sign-wave/store"
+	"github.com/spf13/cobra"
+)
 
 func EmitCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "emit",
 		Short: "Continuously emit records in batches",
+		RunE:  doEmit,
 	}
 
-	cmd.Flags().Uint("batch", 1000, "Batch size")
+	cmd.Flags().Int("batch", 1000, "Batch size")
+	cmd.Flags().String("ingest-url", "http://localhost:"+defaultIngestPort, "Ingest url")
 
 	return cmd
+}
+
+func doEmit(cmd *cobra.Command, args []string) error {
+	db, err := openDB(cmd)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	size, err := cmd.Flags().GetInt("batch")
+	if err != nil {
+		return err
+	}
+
+	for {
+		found, err := store.NewRecord(db).FindAvailableRecords(size)
+		if err != nil {
+			fmt.Println("Error finding records: ", err)
+		}
+		fmt.Println("FOUND:")
+		fmt.Println(found)
+		time.Sleep(1 * time.Second)
+	}
 }
--- ingest_cmd.go
+++ ingest_cmd.go
@@ -2,13 +2,15 @@ package main
 
 import "github.com/spf13/cobra"
 
+const defaultIngestPort = "9876"
+
 func IngestCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "ingest",
 		Short: "Ingest records from an http endpoint",
 	}
 
-	cmd.Flags().String("port", "9876", "Port to bind http server")
+	cmd.Flags().String("port", defaultIngestPort, "Port to bind http server")
 
 	return cmd
 }
